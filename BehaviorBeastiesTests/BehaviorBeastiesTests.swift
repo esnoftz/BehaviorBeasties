b3fbf771c867977425e6d35f8482da4c3a7fe4d6
esnoftz <esnoftz@gmail.com>
2025-02-28 07:31:03 -0600

    Initial Commit

---
+++ BehaviorBeastiesTests/BehaviorBeastiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  BehaviorBeastiesTests.swift
+//  BehaviorBeastiesTests
+//
+//  Created by EVANGELINE NOFTZ on 2/28/25.
+//
+
+import Testing
+@testable import BehaviorBeasties
+
+struct BehaviorBeastiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
