10fe489390c170a8b0f14ef0ea6cfcc12add2c5d
esnoftz <esnoftz@gmail.com>
2025-03-06 07:50:53 -0600

    Added stuff.

--- BehaviorBeasties/ParentPasswordViewController.swift
+++ BehaviorBeasties/ParentPasswordViewController.swift
@@ -21,15 +21,17 @@ class ParentPasswordViewController: UIViewController {
     }
     
     @IBAction func submitYearAction(_ sender: UIButton) {
+        errorLabel.text = ""
         if birthYearOutlet.text == "" {
             errorLabel.text = "Enter your birth year!"
         } else {
-            if Int(birthYearOutlet.text!)! >= 2007 {
-                // put segue here
+            if Int(birthYearOutlet.text!)! >= 1909 && Int(birthYearOutlet.text!)! <= 2007 {
+                performSegue(withIdentifier: "parentChoiceSegue", sender: nil)
             } else {
                 errorLabel.text = "Invalid year!"
             }
         }
+        birthYearOutlet.text = ""
         
     }
     
