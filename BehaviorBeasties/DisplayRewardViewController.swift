37cac64f72a1fc4b2e5ef8daf7ff1e1d2a6bc4d5
esnoftz <esnoftz@gmail.com>
2025-03-13 08:05:25 -0500

    i tried.

---
+++ BehaviorBeasties/DisplayRewardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DisplayRewardViewController.swift
+//  BehaviorBeasties
+//
+//  Created by EVANGELINE NOFTZ on 3/13/25.
+//
+
+import UIKit
+
+class DisplayRewardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
