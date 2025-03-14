d210f1539c855d4feca9e4196289aa3dd87aa7c3
esnoftz <esnoftz@gmail.com>
2025-03-14 07:56:01 -0500

    VC stuff

---
+++ BehaviorBeasties/BankViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BankViewController.swift
+//  BehaviorBeasties
+//
+//  Created by EVANGELINE NOFTZ on 3/14/25.
+//
+
+import UIKit
+
+class BankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ BehaviorBeasties/GoalsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GoalsViewController.swift
+//  BehaviorBeasties
+//
+//  Created by EVANGELINE NOFTZ on 3/14/25.
+//
+
+import UIKit
+
+class GoalsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
